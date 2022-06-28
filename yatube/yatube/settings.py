534fd4dc23c889907ad767055764f061ba081e1d
Филимонова Екатерина <filimonka@yandex.ru>
2022-06-28 11:42:52 +0300

    Add a static dir

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'testserver',
     '[::1]',
+    'fromViolistToPythonist.pythonanywhere.com',
+    'www.fromViolistToPythonist.pythonanywhere.com',
 ]
 
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -151,3 +155,6 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
